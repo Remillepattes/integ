caf901cadfd7507603c45ae435b35c1df54874d1
RMLaroche <RMLaroche@users.noreply.github.com>
2020-01-30 18:02:23 +0100

    new file:   src/main/java/client/Service/Panier.java 	new file:   src/test/java/Service/PanierTest.java

---
+++ src/main/java/client/Service/Panier.java
@@ -0,0 +1,27 @@
+package client.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+
+import client.Entity.Product;
+import client.dao.ProductDAO;
+
+public class Panier {
+	protected List<Product> products = new ArrayList<Product>();
+	
+	public void addProduct(Product product) {
+		products.add(product);
+	}
+	public List<Product> getProducts() {
+		return products;
+	}
+	public double getPrice() {
+		double price = 0;
+		for (Product product : products) {
+			price+=product.getPrice_product();
+		}
+		return price;
+	}
+}
---
+++ src/test/java/Service/PanierTest.java
@@ -0,0 +1,40 @@
+package Service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import client.Entity.Product;
+import client.Service.Panier;
+
+public class PanierTest {
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void getPriceTest() {
+		Product parbrise = new Product();
+		parbrise.setDesc_product("1");
+		parbrise.setName_product("Parbrise");
+		parbrise.setPrice_product(112);
+		
+		Product retroviseur = new Product();
+		parbrise.setDesc_product("2");
+		parbrise.setName_product("Rétroviseur");
+		parbrise.setPrice_product(40);
+		
+		
+		Panier panier = new Panier();
+		panier.addProduct(parbrise);
+		panier.addProduct(retroviseur);
+		assertEquals(112+40, panier.getPrice());
+	}
+		
+}
